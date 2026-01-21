401a58fc49a8b30d91f01136bb45ec07e07357d2
santiago_valenzuela <svalenzuela@nexura.com>
2026-01-21 15:13:29 -0500

    Correccion de app.py

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from src.routes.operations import ops_bp
 
 app = Flask(__name__)
 
-# Registro de Blueprints
-app.register_blueprint(ops_bp, url_prefix='/api/v1')
+# Ruta raíz para probar si el servicio responde algo
+@app.route('/')
+def index():
+    return jsonify({"message": "Servidor activo"}), 200
 
-@app.route('/health', methods=['GET'])
+@app.route('/health')
 def health():
     return jsonify({"status": "up", "service": "external-db-connector"}), 200
+
+app.register_blueprint(ops_bp, url_prefix='/api/v1')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
